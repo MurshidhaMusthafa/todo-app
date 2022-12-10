b67199a78c9a16c7b84057c23b9cf4204aab90ce
Murshidha <murshidhamusthafapt@gmail.com>
2022-12-10 15:55:35 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)}type="text" placeholder="Add item..." />
+        <button onClick={()=>setToDos([...toDos,toDo])}>ADD</button>
+      </div>
+      <div className="todos">
+        {toDos.map((value)=>{ 
+          return(
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>{value}</p>
+          </div>
+          <div className="right">
+          <button onClick={(e)=>{
+                setToDos(toDos.filter(value2 => {
+                  let temp;
+                  if (value.id != value.id){
+                    temp = value2
+                  }
+                  return temp;
+                }));
+              }} >Delete</button>
+          </div>
+        </div>)})}
+      </div>
     </div>
-  );
+
+    </div>
+  )
 }
 
-export default App;
+export default App
